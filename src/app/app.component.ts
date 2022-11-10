56ff1c972dabeeafd1b13623d9ab26f4bbbb34bc
justcoddev <edisonpaulcrz@gmail.com>
2022-11-10 14:06:29 -0500

    @Output() nuevacita = new EventEmitter<any>();

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'citas';
+  listCitas: any[] = [];
+
+  agregarCita(cita: any) {
+    this.listCitas.push(cita);
+    console.log(this.listCitas);
+
+  }
 }
--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-crear-cita',
@@ -13,6 +13,7 @@ export class CrearCitaComponent implements OnInit {
   sintomas = '';
 
   formularioIncorrecto = false;
+  @Output() nuevacita = new EventEmitter<any>();
 
   constructor() { }
 
@@ -33,6 +34,7 @@ export class CrearCitaComponent implements OnInit {
       sintomas: this.sintomas
     }
     console.log((cita));
+    this.nuevacita.emit(cita);
     this.resetCampos();
   }
 
