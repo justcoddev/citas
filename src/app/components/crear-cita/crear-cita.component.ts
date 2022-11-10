8031b8aa978a8e2acdd119d441896d68ae2a10f6
justcoddev <edisonpaulcrz@gmail.com>
2022-11-10 13:46:19 -0500

    TS agg cita

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCitaComponent implements OnInit {
 
+  nombre = '';
+  fecha = '';
+  hora = '';
+  sintomas = '';
+
+  formularioIncorrecto = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  agregarCita() {
+    if (this.nombre == '' || this.fecha == '' || this.hora == '' || this.sintomas == '') {
+      this.formularioIncorrecto = true;
+      return;
+    }
+    this.formularioIncorrecto = false;
+    // Creamos object para enviar al padre
+    const cita = {
+      nombre: this.nombre,
+      fecha: this.fecha,
+      hora: this.hora,
+      sintomas: this.sintomas
+    }
+    console.log((cita));
+    this.resetCampos();
+  }
+
+  resetCampos() {
+    this.nombre = '';
+    this.fecha = '';
+    this.hora = '';
+    this.sintomas = '';
+  }
 }
