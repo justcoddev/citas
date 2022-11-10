4787791dbef18c099785e9e2390956096466d907
justcoddev <edisonpaulcrz@gmail.com>
2022-11-10 01:05:55 -0500

    component crear-cita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { CrearCitaComponent } from './components/crear-cita/crear-cita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListCitasComponent
+    ListCitasComponent,
+    CrearCitaComponent
   ],
   imports: [
     BrowserModule,
